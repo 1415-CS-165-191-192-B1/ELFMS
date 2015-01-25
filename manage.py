3bb7dc0d1bdc47e3eed3a54b51559035667f31b8
Chris Statzer <Chris.Statzer@gmail.com>
2015-01-25 10:12:13 +0800

    Fully documented the manage.py and settings.py

--- manage.py
+++ manage.py
@@ -1,4 +1,81 @@
 #!/usr/bin/env python
+"""
+This is the main entry point for a Django application. All it really does is set
+up the environment variables you need to get the app running. Our project is
+called `twanger`, and it is a normal Python module (it has a __init__.py), and
+it has a settings.py. Here we tell Django that we want to use that settings file
+as the entry for our app. The rest is built into Django's internals. This script
+ultimately just tells django where our configuration is and prints out or parses
+all the commands that are issued from the command line.
+
+Running manage on it's own should output something like:
+
+09:51 $ ./manage.py
+Usage: manage.py subcommand [options] [args]
+
+Options:
+  -v VERBOSITY, --verbosity=VERBOSITY
+                        Verbosity level; 0=minimal output, 1=normal output,
+                        2=verbose output, 3=very verbose output
+  --settings=SETTINGS   The Python path to a settings module, e.g.
+                        "myproject.settings.main". If this isn't provided, the
+                        DJANGO_SETTINGS_MODULE environment variable will be
+                        used.
+  --pythonpath=PYTHONPATH
+                        A directory to add to the Python path, e.g.
+                        "/home/djangoprojects/myproject".
+  --traceback           Raise on exception
+  --version             show program's version number and exit
+  -h, --help            show this help message and exit
+
+Type 'manage.py help <subcommand>' for help on a specific subcommand.
+
+Available subcommands:
+
+[auth]
+    changepassword
+    createsuperuser
+
+[django]
+    check
+    cleanup
+    compilemessages
+    createcachetable
+    dbshell
+    diffsettings
+    dumpdata
+    flush
+    inspectdb
+    loaddata
+    makemessages
+    runfcgi
+    shell
+    sql
+    sqlall
+    sqlclear
+    sqlcustom
+    sqldropindexes
+    sqlflush
+    sqlindexes
+    sqlinitialdata
+    sqlsequencereset
+    startapp
+    startproject
+    syncdb
+    test
+    testserver
+    validate
+
+[sessions]
+    clearsessions
+
+[staticfiles]
+    collectstatic
+    findstatic
+    runserver
+
+"""
+
 import os
 import sys
 
--- twanger/settings.py
+++ twanger/settings.py
@@ -1,63 +1,89 @@
 """
-Django settings for twanger project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
+This is our project configuration file. This is imported at runtime and the
+values are used to run the app. I will document the items below the best I can.
+This is an almost default version of the file and includes everything you need
+to get started.
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
+# Random key used to salt the crypto routines and session encryption.
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '57_=o-j*fq=)bsy_(&i#-2d21t5m)(79(pc)6fny2a5mm%u$n0'
 
+
+# This puts the application in debug mode. You will get fancy error pages with
+# stacktraces
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# Provides extra debug output on template errors
 TEMPLATE_DEBUG = True
 
+# A list of hosts that are allowed to access the app. This is REQUIRED if you
+# dont have DEBUG = True.
 ALLOWED_HOSTS = []
 
-
-# Application definition
-
+# A tuple of all the Django applications we are going to use.
 INSTALLED_APPS = (
+    # These are apps that are built into django:
+
+    # The admin interface
     'django.contrib.admin',
+    # The authentication backend that provides the User model
     'django.contrib.auth',
+    # The contenttypes framework. Allows arbitrary linking of models.
     'django.contrib.contenttypes',
+    # The session framework does all the session handling for you
     'django.contrib.sessions',
+    # A messaging app used by the admin interface.
     'django.contrib.messages',
+    # An app that helps serve static files on a configurable path.
     'django.contrib.staticfiles',
 
+    # Here are the apps that we have written:
+
+    # The site app holds the homepage and auth stuff. Basically stuff that
+    # don't fit anywhere else or are generic.
     'twanger.site',
+    # The app that allows posting messages in twanger.
     'twanger.message',
 )
 
+
+# The `middleware` classes are modules that ALL requests are sent through
+# before you execute the view. These manipulate the request providing
+# more functionality for the view to use.
 MIDDLEWARE_CLASSES = (
+    # Handles all the session stuff in the request.
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # Handles url rewriting and auto `/` additions to the ends of urls.
     'django.middleware.common.CommonMiddleware',
+    # Provides per request helpers to protect from Cross-Site Forgery Requests.
     'django.middleware.csrf.CsrfViewMiddleware',
+    # Middleware for utilizing Web-server-provided authentication.
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # Stores temporary messages. Mostly used by the admin interface.
     'django.contrib.messages.middleware.MessageMiddleware',
+    # Provides click-jacking protection to requests.
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+# The root urlconf is where you define the base urls of the entire application.
+# From here you can include other urlconfs from applications.
 ROOT_URLCONF = 'twanger.urls'
 
+# A auto generated file like manage.py that provides a WSGI interface to your
+# project.
 WSGI_APPLICATION = 'twanger.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
+# Here we are using the most basic database type. SQLite is a limited version
+# of the SQL database standard that stores it's data in a simple file.
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -67,19 +93,16 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
-
+# These enable and configure the various internationalization modules provided
+# by Django.
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+# This is the url where all your static media files will be served from.
 STATIC_URL = '/static/'
