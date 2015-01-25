314f0452ab497991d4b1a01d6e0358406900a175
Chris Statzer <Chris.Statzer@gmail.com>
2015-01-25 08:43:52 +0800

    Added message form and template tags for messaging.

--- twanger/message/templatetags/message_lists.py
+++ twanger/message/templatetags/message_lists.py
@@ -1,7 +1,7 @@
 from django import template
 
 from twanger.message.models import Message
-
+from twanger.message.forms import MessageForm
 
 register = template.Library()
 
@@ -9,6 +9,9 @@ register = template.Library()
 def render_message(message):
     return {'message': message}
 
+@register.inclusion_tag('message/message_form.html')
+def render_message_form():
+    return {'message_form': MessageForm()}
 
 @register.inclusion_tag('message/newest_tag.html')
 def newest_messages(count):
