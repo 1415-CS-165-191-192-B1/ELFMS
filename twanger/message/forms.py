9c9429b394c784b8e74211c1f7fb07c323769b1c
Dyosa Andaca <mariaaprilrose@gmail.com>
2015-03-18 23:15:28 +0800

    yolo

--- twanger/message/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class MessageForm(forms.Form):
-    """A simple form using a Textarea widget. see twanger/site/forms.py
-    for more documentation.
-    """
-    content = forms.CharField(max_length=140, widget=forms.Textarea)
