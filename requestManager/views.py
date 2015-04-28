45757567c0b6720a2e59d0fead280524244d9efb
shen <maraoshen@gmail.com>
2015-04-28 23:05:05 +0800

    lahat

---
+++ requestManager/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from requestManager.models import resource
+from requestManager.forms import resourceForm
+
+# Create your views here.
+def reqManager(request):
+    latest_request = resource.objects.order_by('-creation_date')[:10]
+    context = {'latest_request': latest_request}
+    return render(request, 'requestManager/index.html', context)
+    
+def detail(request, resource_id):
+    r = get_object_or_404(resource, pk=resource_id)
+    return render(request, 'requestManager/detail.html', {'r': r})
+
+def createRequest(request):
+	if request.method == 'POST':
+		form = resourceForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('success/')
+	else:
+		print "get method"
+		form = resourceForm()
+	return render(request, 'requestManager/create.html', {'form':form})
+
+def createSuccess(request):
+	return render(request, 'requestManager/success.html')
