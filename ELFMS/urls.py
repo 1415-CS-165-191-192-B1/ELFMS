5629fa69dd62bc035caa8e4176f694b957ec6461
Andie Rabino <rabinoandie@gmail.com>
2015-06-04 22:20:53 +0800

    fixed redirection of homepage to login view

--- ELFMS/urls.py
+++ ELFMS/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ELFMS.views.home', name='home'),
+    url(r'^$', "login.views.login", name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^request/', include('requestManager.urls', namespace="request")),
     url(r'^admin/', include(admin.site.urls)),
